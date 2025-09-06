8a24ba0b3dd7d4b57a4c0780f5efd60c04bec3e0
VladNkov <vladnkov88@gmail.com>
2025-09-06 09:12:01 +0200

    first commit

---
+++ solbot.py
@@ -0,0 +1,50 @@
+import os
+import time
+import requests
+import telebot
+from dotenv import load_dotenv
+
+load_dotenv()
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+CHAT_ID = os.getenv('CHAT_ID')
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+url = "https://api.binance.com/api/v3/ticker/price"
+params = {"symbol": "SOLUSDT"}
+
+last_price = None
+
+while True:
+    try:
+        response = requests.get(url, params=params)
+        data = response.json()
+        price = float(data["price"])
+
+        if last_price is None:
+            last_price = price
+            bot.send_message(CHAT_ID, f"Стартовый курс SOL/USDT: {price:.2f}")
+            print(f"Стартовый курс SOL/USDT: {price:.2f}")
+        else:
+            diff = abs(price - last_price)
+            if diff >= 0.5 and price > last_price:
+                bot.send_message(
+                    CHAT_ID,
+                    f"🟢🔺 Курс ВЫРОС!\nБыло: {last_price:.2f}\nСтало: {price:.2f}\nИзменение: {diff:.2f} USDT"
+                )
+                print(f"🟢🔺 Курс ВЫРОС!\nБыло: {last_price:.2f}\nСтало: {price:.2f}\nИзменение: {diff:.2f} USDT")
+                last_price = price
+            elif diff >= 0.5 and price < last_price:
+                bot.send_message(
+                    CHAT_ID,
+                    f"🔴🔻 Курс УПАЛ!\nБыло: {last_price:.2f}\nСтало: {price:.2f}\nИзменение: {diff:.2f} USDT"
+                )
+                print(f"🔴🔻 Курс УПАЛ!\nБыло: {last_price:.2f}\nСтало: {price:.2f}\nИзменение: {diff:.2f} USDT")
+                last_price = price
+
+
+        time.sleep(30)
+
+    except Exception as e:
+        bot.send_message(CHAT_ID, f"Ошибка: {e}")
+        time.sleep(30)
